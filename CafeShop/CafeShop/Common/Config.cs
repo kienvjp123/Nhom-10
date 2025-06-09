a3b085bbd6fbbfe4eed83888f51dc1119313e151
kienvjp123 <tk1592003@gmail.com>
2025-06-09 14:03:02 +0700

    commit all

---
+++ CafeShop/CafeShop/Common/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace CafeShop.Common
+{
+    public static class Config
+    {
+        public static string Connection()
+        {
+            /*string conn = @"Data Source=LMK205\SQLEXPRESS;Initial Catalog=cafe;User ID=sa;Password=Leminhkhoi2003;Trust Server Certificate=True";*/
+
+            string conn = @"Server = localhost\SQLEXPRESS;Database = cafe; Trusted_Connection = True; Trust Server Certificate=True";
+            return conn;
+        }
+
+        public static string ImageUrl()
+        {
+            string imageUrl = @"https://localhost:44368/images/";
+            return imageUrl;
+        }
+
+    }
+}
